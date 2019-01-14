f19929276b9bcf22ef10afe7baed2107f7b832ef
DABodr <46652797+DABodr@users.noreply.github.com>
2019-01-14 22:16:42 +0100

    Config.sh
    Generate and configure basics configuration files ( use After Installation )

---
+++ Config.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+NORMAL="\e[0m"
+RED="\e[91m"
+
+which sudo
+if [ "$?" == "0" ]
+then
+    echo "Press Ctrl-C to abort installation"
+    echo "or Enter to proceed"
+
+    read
+else
+    echo -e $RED
+    echo -e "Please install sudo first $NORMAL using"
+    echo " apt-get -y install sudo"
+    exit 1
+fi
+
+set -e
+
+echo "Modification du fichier de configuration Supervisor"
+if [ "$?" == "0" ]
+then
+    echo "Press Ctrl-C to abort installation"
+    echo "or Enter to proceed"
+
+    read
+sudo /etc/init.d/supervisor stop
+sudo cp -v supervisord.conf /etc/supervisor/supervisord.conf
+echo -e "[$GREENOK$NORMAL]"
+echo
+echo "Redemarrage de Supervisor"
+sudo /etc/init.d/supervisor start
+echo
+echo -e "[$GREENOK$NORMAL]"
+echo "Coincoin"
+fi
