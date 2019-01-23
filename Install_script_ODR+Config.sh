c2de3dc42f7ef0b0ce250168a061b2b5a295f335
RSAP <46652797+DABodr@users.noreply.github.com>
2019-01-23 19:09:49 +0100

    Update and rename Install_script_ODR.sh to Install_script_ODR+Config.sh
    Add Config.sh script after Installation Process ( All in One Script ).

--- Install_script_ODR+Config.sh
+++ Install_script_ODR+Config.sh
@@ -304,5 +304,64 @@ echo -e "for compiling the other tools, please see in the ODRinstaller.sh"
 echo -e "script what options are used. Please also read the README"
 echo -e "and INSTALL files in the repositories. $NORMAL"
 echo
+read -r -p "Voulez Vous lancer le Script de Configuration ? [Y/n]" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+   set -e
+
+echo "Modification des fichiers de configuration Supervisor"
+if [ "$?" == "0" ]
+then
+    echo "Press Ctrl-C to abort Supervisor Configuration"
+    echo "or Enter to proceed"
+
+    read
+sudo /etc/init.d/supervisor stop
+sudo cp -v supervisord.conf /etc/supervisor/supervisord.conf
+echo -e "[$GREEN OK $NORMAL]"
+echo
+
+echo " Copie des fichiers de configuration"
+sudo cp -R config /home/$USER/dab/
+sudo sudo chmod -R 777  /home/$USER/dab/config
+echo
+echo -e "[$GREEN OK $NORMAL]"
+echo
+
+echo "Redemarrage de Supervisor"
+sudo /etc/init.d/supervisor start
+echo
+echo -e "[$GREEN OK $NORMAL]"
+echo
+
+echo "Modification de la variable USER des fichiers de configuration"
+for file in /home/$USER/dab/config/supervisor/*.conf
+do
+  echo "Traitement de $file ..."
+  sudo sed -i -e "s/azerty/$USER/g" "$file"
+done 
+echo -e "[$GREEN OK $NORMAL]"
+
+echo "Creation des liens symboliques"
+if [ -f /etc/supervisor/conf.d/mux.conf ]
+then
+sudo rm /etc/supervisor/conf.d/enc-*.conf
+sudo rm /etc/supervisor/conf.d/mux.conf
+fi
+sudo ln /home/$USER/dab/config/supervisor/*.conf /etc/supervisor/conf.d/
+sudo supervisorctl reread 
+sudo supervisorctl update
+echo
+echo "Ouverture de la page http://localhost:8001 dans votre navigateur "
+echo " utilisateur: odr  mot de passe: odr"
+echo " ctrl+c pour quitter"
+echo " pensez a ajouer cette page a vos favoris"
+sensible-browser http://localhost:8001 &
+echo 
+echo
+echo -e "[$GREEN OK $NORMAL]"
+echo
+fi 
+fi
 echo
 
