cb8cbdf34958936c950b73d90d4707dfcdd7a3ae
RSAP <46652797+DABodr@users.noreply.github.com>
2020-02-11 00:37:49 +0100

    Update listen_my_mux.sh

--- config/listen_my_mux.sh
+++ config/listen_my_mux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/$USER/dab/mmbtools-aux/zmqtest/zmq-sub/zmq-sub localhost 18081 | /home/$USER/dab/dablin/build/src/dablin_gtk
+/home/$USER/dab/mmbtools-aux/zmqtest/zmq-sub/zmq-sub localhost 18081 | /home/$USER/dab/dablin/build/src/dablin_gtk -s 0x4DB0
